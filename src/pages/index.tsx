e9b7937711e91b2f932befc9e7ab4100ab9dc422
WagnerLeodoro <wagner.augustoleodoro@gmail.com>
2023-04-02 00:03:59 -0300

    Página home estática

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,24 @@
+import { HomeContainer, Product } from "@/styles/pages/home";
+import Image from "next/image";
+
+import Camiseta1 from "../assets/camisetas/1.png";
+import { Handbag } from "@phosphor-icons/react";
+
 export default function Home() {
-  return <h1>Hello World!</h1>;
+  return (
+    <HomeContainer>
+      <Product href="/">
+        <Image src={Camiseta1} width={520} height={480} alt="" />
+        <footer>
+          <div className="product-info">
+            <strong>Camiseta Beyond the Limits</strong>
+            <span>R$ 79,90</span>
+          </div>
+          <button className="cart-icon">
+            <Handbag size={32} weight="bold" color="white" />
+          </button>
+        </footer>
+      </Product>
+    </HomeContainer>
+  );
 }
---
+++ src/styles/pages/home.ts
@@ -0,0 +1,92 @@
+import { styled } from "..";
+import Link from "next/link";
+
+export const HomeContainer = styled("main", {
+  display: "flex",
+  width: "100%",
+  maxWidth: "calc(100vw - ((100vw - 1180px) / 2))",
+  marginLeft: "auto",
+});
+
+export const Product = styled(Link, {
+  background: "linear-gradient(180deg, #1ea483 0%, #7465d4 100%)",
+  borderRadius: 8,
+  cursor: "pointer",
+  position: "relative",
+  overflow: "hidden",
+
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+
+  img: {
+    objectFit: "cover",
+  },
+
+  footer: {
+    position: "absolute",
+    bottom: "0.25rem",
+    left: "0.25rem",
+    right: "0.25rem",
+    padding: "2rem",
+
+    borderRadius: 6,
+
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+
+    backgroundColor: "rgba(0, 0, 0, 0.6)",
+
+    transform: "translateY(110%)",
+    opacity: 0,
+    transition: "all 0.2s ease-in-out",
+
+    ".product-info": {
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "flex-start",
+      padding: 0,
+      gap: 4,
+
+      width: 252,
+      height: 70,
+    },
+
+    strong: {
+      fontSize: "$lg",
+      color: "$gray100",
+    },
+
+    span: {
+      fontSize: "$xl",
+      fontWeight: "bold",
+      color: "$green300",
+    },
+
+    ".cart-icon": {
+      display: "flex",
+      flexDirection: "row",
+      alignItems: "center",
+      padding: 12,
+      gap: 12,
+      border: 0,
+      color: "white",
+      cursor: "pointer",
+
+      width: 56,
+      height: 56,
+
+      background: "$green300",
+      borderRadius: 6,
+    },
+  },
+
+  "&:hover": {
+    footer: {
+      transform: "translateY(0%)",
+      opacity: 1,
+    },
+  },
+});
